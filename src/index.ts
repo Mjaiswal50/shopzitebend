2a326dbe5c8701ff82496125ad495eb5b06b70f0
kishori-pathrabe <kishoripathrabe123@gmail.com>
2024-01-07 05:17:02 +0530

    DONE: added environment file and index file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,22 @@
 import * as express from 'express';
-let app: express.Application = express();
-app.listen(5000, () =>{
-    console.log("server started");
+import mongoose from 'mongoose';
+let app:express.Application =  express(); //constructor is called(express());
+app.listen(5000,() =>{
+    console.log("server is running at port 5000");
+}); // listens the server with port no: 5000
+
+app.get('/login',(req:any,res,next) =>{
+    console.log("middleware",req.query);
+    res.send({msg:"Successfully done"})
 })
+
+const MONGO_URI: string = 'mongodb+srv://kishori:Kish1998@cluster0.o07oh.mongodb.net/?retryWrites=true&w=majority';
+mongoose.connect(MONGO_URI).then(() => {
+    console.log('connected to database');
+}).catch(err => {
+    console.log('error hai', err);
+});
+
+//CONSOLE OUTPUT :
+//server is running at port 5000
+//connected to database
