e48cf4a68ea63e0dee2a010a3cbfd0ff473df785
Mayank Jaiswal <jaiswalmayank450@gmail.com>
2024-12-11 15:10:39 +0530

    Done:Base Restart

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,4 +1,3 @@
-import { DevEnvironment } from './environment.dev';
 import { ProdEnvironment } from './environment.prod';
 
 export  * from './environment.prod'
@@ -6,5 +5,5 @@ export function getEnvironmentVariable() {
     if(process.env.NODE_ENV ==='production'){
         return ProdEnvironment;
     }
-    return DevEnvironment;
+    return ProdEnvironment;
 }
